3afe6b349dc0e79dcbc0d68a44ff61b35120a869
mermakov <mermakov@geeksforless.net>
2016-09-09 10:06:01 +0300

    obj add

---
+++ app/src/main/obj/local/armeabi/objs/smoker/smoke.o.d
@@ -0,0 +1,43 @@
+obj/local/armeabi/objs/smoker/smoke.o: jni/smoke.cpp \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/smoke.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/galois.h \
+ /Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/list \
+ /Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/tuple \
+ /Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/set \
+ /Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-errno.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-version.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-unix.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-threadpool.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-linux.h \
+ /Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/pthread-barrier.h
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/smoke.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/galois.h:
+
+/Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/list:
+
+/Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/tuple:
+
+/Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/set:
+
+/Users/max_ermakov/Library/Android/sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-errno.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-version.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-unix.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-threadpool.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/uv-linux.h:
+
+/Users/max_ermakov/AndroidProjects/SmokeTunnel/app/src/main/jni/include/pthread-barrier.h:
